70856286b9162b072408a513f516a40f70b5219a
Fienhold, Elias <Elias.Fienhold@adesso.de>
2021-11-25 11:17:06 +0100

    add SQL SCRIPT

--- DB_SQL_SCRIPT/Webshop_DB.sql
+++ DB_SQL_SCRIPT/Webshop_DB.sql
@@ -1,4 +1,4 @@
 Drop database if exists xampp;
 create database xampp;
 use xampp;
-create table artikel (`artikelid` int,`Bezeichnung` varchar(40));
+create table artikel (`artikelid` int,`Bezeichnung` varchar(40),`preis` decimal(19,4),`beschreibung` varchar(40));

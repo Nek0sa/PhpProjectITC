5872dd7584ed5abb26b53ebfc984b106ffb5b14a
Fienhold, Elias <Elias.Fienhold@adesso.de>
2021-12-16 10:19:29 +0100

    wip

--- Html_Php/login.php
+++ Html_Php/login.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+if (array_key_exists('User', $_POST))
+?>
 <html>
 <head>
     <link href="../css/styles.css" rel="stylesheet"/>
@@ -5,15 +10,19 @@
 <body>
 <?php include("Templates/nav.php") ?>
 <div style=" height: 100%;width: 100%;display: flex;align-items: center;justify-content: center">
+    <p>Account login</p>
     <div style="border-color: #5c636a;border-style: solid;">
         <form method="post" action="PHP_Process/process_login.php">
-
-            <p>Account login</p>
-            <div>Benutzername: <input type="text"></div>
-            <div>Password:<input type="password"></div>
-            <input type="submit">
-
-
+            <input type="hidden" name="form_send"/>
+            <div class="form-group">
+                <label class="form-group" for="User">Benutzername</label>
+                <input type="text" class="form-control" id="User" name="User">
+            </div>
+            <div class="form-group">
+                <label class="control-label" for="Password" >Password</label>
+                <input type="password" class="form-control" id="Password" name="Password">
+            </div>
+            <button type="submit">anmelden</button>
         </form>
     </div>
 </div>

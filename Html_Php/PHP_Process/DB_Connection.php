5055b2374f983c55de6f3bc3976204815aec7ffb
savitsky <tsavitsky@yahoo.de>
2022-01-12 17:59:24 +0100

    added ShoppingCart (empty)

--- Html_Php/PHP_Process/DB_Connection.php
+++ Html_Php/PHP_Process/DB_Connection.php
@@ -3,7 +3,7 @@ function connectToDatabase ():mysqli {
 $servername = "localhost";
 $username = "root";
 $password = "Roottest";
-$dbname = "webshop";
+$dbname = "xampp";
 // Create connection
 return new mysqli($servername, $username, $password,$dbname);
 }

49fb7752c66f5bb8b9c7582b23da147b7603cdaf
Bene <bene0287@gmail.com>
2022-01-13 12:19:16 +0100

    Datenbank

--- DB_SQL_SCRIPT/Webshop_DB.sql
+++ DB_SQL_SCRIPT/Webshop_DB.sql
@@ -1,7 +1,4 @@
-Drop database if exists xampp;
-create database xampp;
-use xampp;
-create table artikel (`artikelid` int,`Bezeichnung` varchar(40),`preis` decimal(19,4),`beschreibung` varchar(40));
+
 
 Drop database if exists webshop;
 CREATE SCHEMA `webshop` ;

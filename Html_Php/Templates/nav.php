065951d1475b09b85847d40c5f893a4aa12b26c8
Fienhold, Elias <Elias.Fienhold@adesso.de>
2022-01-13 12:17:14 +0100

    nav warenkorb anzahl

--- Html_Php/Templates/nav.php
+++ Html_Php/Templates/nav.php
@@ -20,7 +20,7 @@
                 <button class="btn btn-outline-dark" type="submit">
                     <i class="bi-cart-fill me-1"></i>
                     Cart
-                    <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                    <span class="badge bg-dark text-white ms-1 rounded-pill"><?php echo $_SESSION['DingeImWarenkorb'] ?></span>
                 </button>
             </form>
         </div>
